4556608b6caf927d22add2ed5ca3fb147aacac0a
RyanVNgo <vanryan711@gmail.com>
2024-07-04 15:42:33 -0700

    Adjusted compile implementation

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CXX = gcc
-CXXFLAGS = -Iinc $(foreach dir, $(wildcard lib/*), -I$(dir)/include)
+CXXFLAGS = -Iinc $(foreach dir, $(wildcard lib/*), -I$(dir))
 LDFLAGS = $(foreach dir, $(wildcard lib/*), -L$(dir)) -lm -lnsl -lz
 
 SRCDIR = src
@@ -8,18 +8,18 @@ LIBDIRS = $(wildcard lib/*)
 LIBS = $(foreach dir, $(LIBDIRS), $(dir)/*.a)
 
 SOURCES = $(wildcard $(SRCDIR)/*.c)
-OBJECTS = $(wildcard $(SRCDIR)/%.c, $(OBJDIR)/%.o, $(SOURCES))
+OBJECTS = $(patsubst $(SRCDIR)/%.c, $(OBJDIR)/%.o, $(SOURCES))
 
 PROGRAM = Asteria
 
-# CFITSIO = -L./lib -4.4.1 -Wl,-rpath,./inc -4.4.1 -lcfitsio -lm
 all: $(PROGRAM)
 
 $(PROGRAM): $(OBJECTS) $(LIBS)
-	$(CC) -o $@ $^ $(CFLAGS) $(LDFLAGS)
+	$(CC) -o $@ $^ $(CXXFLAGS) $(LDFLAGS)
 
 $(OBJDIR)/%.o: $(SRCDIR)/%.c
-	$(CC) -c -o $@ $< $(CFLAGS)
+	@mkdir -p $(OBJDIR)
+	$(CC) -c -o $@ $< $(CXXFLAGS)
 
 clean:
 	rm -rf $(OBJDIR) $(PROGRAM)
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "fitsio.h"
 
 int communicateStatusAndExit(int *status) {
@@ -6,16 +7,29 @@ int communicateStatusAndExit(int *status) {
   exit(*status);
 }
 
+void printHeader(fitsfile*);
+
 int main(int argc, char* argv[]) {
-  
   fitsfile* fptr;
+  int status = 0;
+  if (!argv[1]) return 1; // temporary check for if file name is provided, must properly implement later
+  fits_open_file(&fptr, argv[1], READONLY, &status);
+
+  int c;
+  while ((c = getopt(argc, argv, "h")) != -1) {
+    switch (c) {
+      case 'h':
+        printHeader(fptr);
+        break;
+    }
+  }
+
+  return status;
+}
+
+void printHeader(fitsfile* fptr) {
   char card[FLEN_CARD];
   int status = 0, nkeys, i;
-
-  if (!argv[1]) return 1; // temporary check for if file name is provided, must properly implement later
-
-  // test for cfitsio library installation
-  fits_open_file(&fptr, argv[1], READONLY, &status);
   fits_get_hdrspace(fptr, &nkeys, NULL, &status);
   if (status) communicateStatusAndExit(&status);
   for (i = 1; i <= nkeys; i++) {
@@ -25,6 +39,5 @@ int main(int argc, char* argv[]) {
   printf("END\n\n");
   fits_close_file(fptr, &status);
   if (status) communicateStatusAndExit(&status);
-
-  return status;
+  return;
 }
