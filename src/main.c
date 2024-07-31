37f6d52e50677a2c7626fcaf0c5bfc79f8b0ce0d
RyanVNgo <vanryan711@gmail.com>
2024-07-31 02:23:31 -0700

    Organization adjustment
    Added the compiled executable to the .gitignore file to keep it untracked.
    
    Also clean slate the project.

--- src/main.c
+++ src/main.c
@@ -1,44 +1,7 @@
-#include <stdio.h>
-#include <unistd.h>
-#include "fitsio.h"
-
-int communicateStatusAndExit(int *status) {
-  fits_report_error(stderr, *status);
-  exit(*status);
-}
-
-void printHeader(fitsfile*);
-
+#include <gtk/gtk.h>
 
 int main(int argc, char* argv[]) {
-  fitsfile* fptr;
-  int status = 0;
-  if (!argv[1]) return 1; // temporary check for if file name is provided, must properly implement later
-  fits_open_file(&fptr, argv[1], READONLY, &status);
+  printf("Hello from Asteria!\n");
 
-  int c;
-  while ((c = getopt(argc, argv, "h")) != -1) {
-    switch (c) {
-      case 'h':
-        printHeader(fptr);
-        break;
-    }
-  }
-
-  return status;
-}
-
-void printHeader(fitsfile* fptr) {
-  char card[FLEN_CARD];
-  int status = 0, nkeys, i;
-  fits_get_hdrspace(fptr, &nkeys, NULL, &status);
-  if (status) communicateStatusAndExit(&status);
-  for (i = 1; i <= nkeys; i++) {
-    fits_read_record(fptr, i, card, &status);
-    printf("%s\n", card);
-  }
-  printf("END\n\n");
-  fits_close_file(fptr, &status);
-  if (status) communicateStatusAndExit(&status);
-  return;
+  return 0;
 }
